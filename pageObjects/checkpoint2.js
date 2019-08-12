c644f5901cb93c1c495c1c224ff7e82798e1f05a
MarohnHoward <marohn.howard@gmail.com>
2019-08-12 16:13:59 -0600

    Here you go Andrew

--- pageObjects/checkpoint2.js
+++ pageObjects/checkpoint2.js
@@ -9,12 +9,24 @@ module.exports = {
         selector:'//button[@name="evenOddButton"]',
         locateStrategy: 'xpath'
     },
+    evenResult:{
+        selector: '//span[@name="evenResults"]',
+        locateStrategy: 'xpath'
+    },
+    oddResult: {
+        selector: '//span[@name="oddResults"]',
+        locateStrategy: 'xpath'
+    },
     filterObjectsInput: {
         selector: '//input[@name="objectFilterInput"]',
         locateStrategy: 'xpath'
     },
     filterObjectsBtn: {
         selector: '//button[@name="objectFilterButton"]',
+        locateStrategy: 'xpath'
+    },
+    filterObjectsResult: {
+        selector: '//span[@name="objectFilterResults"]',
         locateStrategy: 'xpath'
     },
     filterStringInput: {
@@ -25,12 +37,20 @@ module.exports = {
         selector: '//button[@id="nameFilterButton"]',
         locateStrategy: 'xpath'
     },
+    filterStringResult: {
+        selector:'//span[@name="nameFilterResults"]',
+        locateStrategy: 'xpath'
+    },
     palindromeInput: {
         selector: '//input[@name="palindromeInput"]',
         locateStrategy: 'xpath'
     },
     palindromeBtn:{
         selector: '//button[@name="palindromeButton"]',
+        locateStrategy: 'xpath'
+    },
+    palindromeResult:{
+        selector: '//span[@name="palindromeResults"]',
         locateStrategy: 'xpath'
     },
     sumInput1:{
@@ -43,6 +63,10 @@ module.exports = {
     },
     sumAddBtn: {
         selector:'//button[@name="sumButton"]',
+        locateStrategy: 'xpath'
+    },
+    sumResult: {
+        selector: '//span[@name="sumResults"]',
         locateStrategy: 'xpath'
     },
 },
